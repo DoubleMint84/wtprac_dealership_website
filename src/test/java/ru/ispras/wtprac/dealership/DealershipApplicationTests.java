efcf949645162d973f212c407e4253d09c596257
Nikita Borodavko <doublem1nt84@ya.ru>
2025-03-01 18:21:33 +0300

    Initialize the Spring project

---
+++ src/test/java/ru/ispras/wtprac/dealership/DealershipApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ispras.wtprac.dealership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealershipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
