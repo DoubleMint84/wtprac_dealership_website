0e04c6f74b70b6aec80aefa33469a2752be0b6fc
Nikita Borodavko <doublem1nt84@ya.ru>
2025-05-04 20:20:11 +0300

    Implement getAllTestDrivesOfClient in TestDriveScheduleDAO

--- src/main/java/ru/ispras/wtprac/dealership/DAO/TestDriveScheduleDAO.java
+++ src/main/java/ru/ispras/wtprac/dealership/DAO/TestDriveScheduleDAO.java
@@ -3,9 +3,7 @@ package ru.ispras.wtprac.dealership.DAO;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
-import ru.ispras.wtprac.dealership.model.Car;
-import ru.ispras.wtprac.dealership.model.TestDriveSchedule;
-import ru.ispras.wtprac.dealership.model.TestDriveStatus;
+import ru.ispras.wtprac.dealership.model.*;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
@@ -32,6 +30,16 @@ public class TestDriveScheduleDAO extends CommonDAO<TestDriveSchedule, Long> {
                 query.setParameter("testDriveStatus", TestDriveStatus.Canceled);
             }
             result = query.getResultList();
+        }
+        return result;
+    }
+
+    public Collection<TestDriveSchedule> getAllTestDrivesOfClient(Client client) {
+        Collection<TestDriveSchedule> result;
+        try (Session session = sessionFactory.openSession()) {
+            result = session.createQuery("from TestDriveSchedule where client = :client", TestDriveSchedule.class)
+                    .setParameter("client", client)
+                    .list().stream().toList();
         }
         return result;
     }
